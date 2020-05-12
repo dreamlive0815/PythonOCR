e5ddabb1b38ee438f91926f635c5d08abbe8724e
Koishi <995928339@qq.com>
2020-05-12 08:50:14 +0800

    202005120850

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ import _thread
 
 threadCount = 3
 
-
 docxDir = Tools.getDocxDir()
 
 cacheDir = Tools.getCacheDir()
@@ -30,26 +29,26 @@ Tools.createDir(logDir)
 docxFiles = (
     # '../多湖街道/七里畈股东清册.docx',
     # '../多湖街道/上古井村股东清册.docx',
-    '../多湖街道/下渎口村股东清册.docx',
-    '../多湖街道/东湄社区股权清册.docx',
+    # '../多湖街道/下渎口村股东清册.docx',
+    # '../多湖街道/东湄社区股权清册.docx',
     # '../多湖街道/东盛村股东清册.docx',
-    '../多湖街道/东龙口社区股东清册.docx',
-    '../多湖街道/十二里村经济合作章程及股东清册.docx',
-    '../多湖街道/四大门村股东清册.docx',
-    '../多湖街道/垄窑村股东清册.docx',
+    # '../多湖街道/东龙口社区股东清册.docx',
+    # '../多湖街道/十二里村经济合作章程及股东清册.docx',
+    # '../多湖街道/四大门村股东清册.docx',
+    # '../多湖街道/垄窑村股东清册.docx',
     # '../多湖街道/庄头社区股东清册.docx',
     # '../多湖街道/新安村股东清册.docx',
-    '../多湖街道/林头社区股东清册.docx',
-    '../多湖街道/樟新村股东清册.docx',
+    # '../多湖街道/林头社区股东清册.docx',
+    # '../多湖街道/樟新村股东清册.docx',
     # '../多湖街道/毛草山股东名册.docx',
     # '../多湖街道/永红村股东清册.docx',
-    '../多湖街道/潭头村股东清册.docx',
-    '../多湖街道/王坦村股权清册.docx',
-    '../多湖街道/王宅埠村股权清册.docx',
+    # '../多湖街道/潭头村股东清册.docx',
+    # '../多湖街道/王坦村股权清册.docx',
+    # '../多湖街道/王宅埠村股权清册.docx',
     # '../多湖街道/西盛村股东名册.docx',
     # '../多湖街道/近宅股东名单.docx',
     # '../多湖街道/雅地村股东清册.docx',
-    '../多湖街道/驿头村股东清册.docx',
+    # '../多湖街道/驿头村股东清册.docx',
     # '../多湖街道/驿头村股东清册修改前.docx',
 )
 
@@ -70,7 +69,11 @@ def doJob(i):
         childFragDir = Tools.joinPath(fragDir, str(j))
         Tools.createDir(childFragDir)
         ocr = OCRTools(childFragDir)
-        tr = ocr.ocr(picPath)
+        try:
+            tr = ocr.ocr(picPath)
+        except Exception as e:
+            tr = ocr.getEmptyData()
+            Log.e(str(e))
         r += tr
 
     excelPath = Tools.joinPath(cDir, '{}.xlsx'.format(name))
@@ -96,8 +99,8 @@ def startThread(tindex):
     except Exception as e:
         Log.e(str(e))
 
-""" for i in range(threadCount):
-    startThread(i) """
+for i in range(threadCount):
+    startThread(i)
 
 input('任意键结束')
 input('任意键结束')
